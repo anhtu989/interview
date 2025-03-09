415388da3d061b828dbd3a0df137dfee8befe951
anhtu989 <edeypetzii@hotmail.com>
2025-03-09 22:25:08 +0800

    refactor(system): transition from callback pattern to async/await

--- DesignPattern/BridgePattern/concrete_implementor.h
+++ DesignPattern/BridgePattern/concrete_implementor.h
@@ -1,3 +1,4 @@
+cpp
 //
 // Created by xiemenghui on 2018/7/21.
 //
@@ -17,8 +18,9 @@ public:
     {
         std::cout << "Light is on." << std::endl;
     }
+
     // Turn off the lights
-    virtual  void PowerOff() override
+    virtual void PowerOff() override
     {
         std::cout << "Light is off." << std::endl;
     }
@@ -33,12 +35,12 @@ public:
     {
         std::cout << "Fan is on." << std::endl;
     }
+
     // Turn off the fan
-    virtual  void PowerOff() override
+    virtual void PowerOff() override
     {
         std::cout << "Fan is off." << std::endl;
     }
 };
-
 
 #endif //DESIGNPATTERN_CONCRETE_IMPLEMENTOR_H
